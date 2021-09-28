54b4421276b2cb79c7fa2df324498011fd20ef85
Dan Diac <danut.diac@thinslices.com>
2021-09-28 09:10:55 +0300

    move commands to new file

---
+++ commands.js
@@ -0,0 +1,129 @@
+const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
+
+const MAX_LIST_LEGTH = 13
+const globalStore = {
+  items: [],
+  connection: null,
+  message: null,
+  channel: null
+}
+
+
+const COMANDS = [{
+    name: 'hai',
+    description: 'Vin unde ești tu.',
+    exec: joinChannel
+  },
+  {
+    name: 'cântă',
+    description: 'Voi cânta ceva, cum știu eu.',
+    exec: play
+  },
+  {
+    name: 'canta',
+    description: 'Tot cânt, dar fără diacritice',
+    exec: play
+  },
+  {
+    name: 'alta',
+    description: 'Opresc cântarea curentă, pentru una mai bună.',
+    exec: playNext
+  },
+  {
+    name: 'listează',
+    description: `Ițî arăt următoarele ${MAX_LIST_LEGTH} melodii din listă.`,
+    exec: listCurrentSongs
+  },
+  {
+    name: 'pleacă',
+    description: 'Plec',
+    exec: disconnect
+  },
+]
+
+
+async function interpretMessage(message) {
+  if (!message.guild) return;
+
+  if (message.member.voice.channel) {
+      globalStore.channel = await message.member.voice.channel
+  } else {
+      return message.reply('You need to join a voice channel to use this bot!');
+  }
+  globalStore.message = message
+
+  COMANDS.forEach(async function (command) {
+    if (message.content.includes(command.name)) {
+      await command.exec()
+    }  
+  })
+}
+
+async function joinChannel() {
+  if (!globalStore.connection) {
+    globalStore.connection = await globalStore.channel.join()
+  }
+}
+
+async function playNext() {
+  globalStore.items.splice(0, 1)
+  await recurrentPlayItems()
+}
+
+async function play() {
+  try {
+      await joinChannel()
+      const playUrl = globalStore.message.content.split(' ')[1]
+      console.log(`Loading ${playUrl}...`, )
+
+      await playPlaylist({playlistUrl: playUrl})
+      listCurrentSongs()
+  } catch (err) {
+      console.log(`Didn't work: ${err}`)
+  }
+}
+
+async function listCurrentSongs() {
+  const songs = globalStore.items.slice(0, MAX_LIST_LEGTH).map(song => song.title)
+  await globalStore.message.reply(songs.join('\n'))
+}
+
+async function playPlaylist({playlistUrl}) {
+  try {
+      let list = await ytpl(playlistUrl, { pages: 100 });
+
+      console.log(`Loaded ${list.items.length} items`)
+      globalStore.items = list.items
+      await recurrentPlayItems()
+  } catch(err) {
+      console.log('Paylist error: ', err)
+  }
+}
+
+async function recurrentPlayItems() {
+  if (globalStore.items.length === 0) {
+      console.log('There are no songs in the list')
+      return
+  }
+
+  const dispatcher = await globalStore.connection.play(ytdl(globalStore.items[0].url), {
+      volume: 0.5,
+  });
+  dispatcher.on('finish', async () => {
+      if (globalStore.items.length > 1) {
+          globalStore.items.splice(0, 1)
+          return recurrentPlayItems()
+      }
+
+      globalStore.connection.leave()
+  });
+}
+
+async function disconnect() {
+  return globalStore.channel.leave()
+}
+
+module.exports = {
+  interpretMessage
+}
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const ytdl = require('ytdl-core');
-const ytpl = require('ytpl');
-
-const MAX_LIST_LEGTH = 13
-const globalStore = {
-    items: [],
-    connection: null
-}
+const { interpretMessage } = require('./commands');
+const { bot_secret_token } = require('./config.json')
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
@@ -16,86 +10,6 @@ client.on('ready', () => {
     })
 })
 
-client.on('message', async (message) => {
-    console.log('conent found', message.content)
-    if (!message.guild) return;
-
-    let channel
-    if (message.member.voice.channel) {
-        channel = await message.member.voice.channel
-    } else {
-        return message.reply('You need to join a voice channel to use this bot!');
-    }
-
-    if (message.content === '/join') {
-        await joinChannel()
-    }
-
-    if (message.content.includes('/play')) {
-        try {
-            await joinChannel()
-            const playUrl = message.content.split(' ')[1]
-            console.log(`Loading ${playUrl}...`, )
-
-            await playPlaylist({playlistUrl: playUrl})
-            listCurrentSongs({message})
-        } catch (err) {
-            console.log(`Didn't work: ${err}`)
-        }
-    }
-
-    if (message.content.includes('/next')) {
-        globalStore.items.splice(0, 1)
-        await recurrentPlayItems()
-    }
-
-    if (message.content.includes('/list')) {
-        listCurrentSongs({message})
-    }
-
-    async function joinChannel() {
-        if (!globalStore.connection) {
-           globalStore.connection = await channel.join()
-        }
-    }
-})
-
-function listCurrentSongs({ message }) {
-    const songs = globalStore.items.slice(0, MAX_LIST_LEGTH).map(song => song.title)
-    message.reply(songs.join('\n'))
-}
-
-async function playPlaylist({playlistUrl}) {
-    try {
-        let list = await ytpl(playlistUrl, { pages: 100 });
-
-        console.log(`Loaded ${list.items.length} items`)
-        globalStore.items = list.items
-        await recurrentPlayItems()
-    } catch(err) {
-        console.log('Paylist error: ', err)
-    }
-}
-
-async function recurrentPlayItems() {
-    if (globalStore.items.length === 0) {
-        console.log('There are no songs in the list')
-        return
-    }
-
-    const dispatcher = await globalStore.connection.play(ytdl(globalStore.items[0].url), {
-        volume: 0.5,
-    });
-    dispatcher.on('finish', async () => {
-        if (globalStore.items.length > 1) {
-            globalStore.items.splice(0, 1)
-            await recurrentPlayItems()
-        }
-    });
-
-}
-
-
-bot_secret_token = "NzYwNzYwNDIyNTMzMDM4MDkx.X3QvWg.7S9IKKPLm27k9yfuQhUSDO1j6GE"
+client.on('message', interpretMessage)
 
 client.login(bot_secret_token)
